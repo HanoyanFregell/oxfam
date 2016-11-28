f81f2ed9daf9bdf3714d8dcfcee3d8ead7ab7c72
Media Team <Media Team@MediaTeam-PC.local>
2016-11-28 16:57:29 +0800

    for Office

--- home.php
+++ home.php
@@ -5,8 +5,9 @@
 
         <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
         <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
-        <link href="css/bootstrap-custom_home.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-custom-home.css" rel="stylesheet" type="text/css"/>
         <link rel="shortcut icon" type="image/png" href="resources/Oxfam_Circle_Green-min.png"/>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet'  type='text/css'>
 
     </head>
     <body>
@@ -17,11 +18,8 @@
                 </div>
 
                 <form class="navbar-form navbar-right">
-                    <div class="input-group has-feedback">
-                         <button type="submit" name="add" class="btn btn-default  add_button">ADD</button>
-                    </div>
-                    <div class="input-group has-feedback">
-                        <input type="text" class="form-control" placeholder="Search">
+                    <div class="input-group has-feedback search-bar-wrapper">
+                        <input type="text" class="form-control search-bar" placeholder="Search">
                         <span class="glyphicon glyphicon-search form-control-feedback"></span>
                     </div>
                 </form>
@@ -37,17 +35,83 @@
         </nav>
 
         <div class="container-fluid">
-             <div class="row text-center" style="border: 1px solid black;padding: 0; height: 100px; margin: 0; ">
-                <div class="col-sm-4" style="border: 1px solid black; height: 100%;padding: 0; margin: 0; ">Orders</div>
-                <div class="col-sm-4" style="border: 1px solid black; height: 100%;padding: 0; margin: 0; ">Stock</div>
-                <div class="col-sm-4" style="border: 1px solid black; height: 100%;padding: 0; margin: 0; ">Users</div>
+            <div class="row text-center top-bar">
+                <div class="col-sm-2 top-notif new">
+                    <p class="top-notif-number">10</p>
+                    <p class="top-notif-desc">New Orders</p>
+                </div>
+                <div class="col-sm-2 top-notif pending" >
+                    <p class="top-notif-number">90</p>
+                    <p class="top-notif-desc">Pending Orders</p>
+
+                </div>
+                <div class="col-sm-2 top-notif  new" >
+                    <p class="top-notif-number">20</p>
+                    <p class="top-notif-desc">New Shipments</p>
+                </div>
+                <div class="col-sm-2 top-notif pending" >
+                    <p class="top-notif-number ">80</p>
+                    <p class="top-notif-desc">Pending Shipments</p>
+                </div>
             </div>
         </div>
-        <div class="container-fluid">
-            <div class="row text-center" style="border: 1px solid black; height: 100%;padding: 0; margin: 0; ">
-                <div class="col-sm-4" style="border: 1px solid black; height: 100%;padding: 0; margin: 0; ">Orders</div>
-                <div class="col-sm-4" style="border: 1px solid black; height: 100%;padding: 0; margin: 0; ">Stock</div>
-                <div class="col-sm-4" style="border: 1px solid black; height: 100%;padding: 0; margin: 0; ">Users</div>
+
+        <div class="container-fluid"  >
+            <div class="row order-list-wrapper" >
+                <div class="col-sm-6 order-list" >
+                    <div class="container-fluid order-list-top">
+                        <div class="col-sm-5  ">
+                            Picture
+                        </div>
+                        <div class="col-sm-4 text-center">
+                            Orders
+                        </div>
+                        <div class="col-sm-3 text-center">
+                            Status
+                        </div>
+                    </div>
+                    <div class="list-group" >
+                        <a href="#" class="list-group-item order-list-item" >
+                            <div class="row" >
+                                <div class="col-sm-2 profile-picture-wrapper" >
+                                    <img class="profile-picture center-block" src="resources/Oxfam_Circle_Green-min.png" id="symbol" />
+                                </div>
+                                <div class="col-sm-3" >
+                                    <div class="row" >
+                                        <div class="col-sm-12 name">
+                                            Cedric Angelo Plasabas
+                                        </div>
+                                    </div>
+                                    <div class="row" >
+                                        <div class="col-sm-12 description" >
+                                            Block 22 Lot 7, Calle de Teresita, Fuente de Villa-Abrille, Tulip Drive, Juna Subdv, Brgy. Matina Crossing, Davao City
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4  orders" >
+                                    <div class="row  text-center">
+                                        <div class="col-sm-6">Orange</div>
+                                        <div class="col-sm-4">x2</div>
+                                    </div>
+                                    <div class="row  text-center">
+                                        <div class="col-sm-6">Banana</div>
+                                        <div class="col-sm-4">x5</div>
+                                    </div>
+                                    <div class="row  text-center">
+                                        <div class="col-sm-6">Orange</div>
+                                        <div class="col-sm-4">x3</div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3 text-center status">
+                                    Not Processed
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <button type="submit" name="add" class="btn btn-default more-order-list-item text-center center-block ">
+                        SHOW MORE ORDERS
+                    </button>
+                </div>
             </div>
         </div>
 
